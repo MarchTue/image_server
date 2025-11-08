84aa445d0a312b961a19f40411c8bbd6f29717f6
YooByWk <tls19190@naver.com>
2025-11-08 23:04:22 +0900

    Feat #2 : Add Image Module's controller and service
    - 단순 생성

--- image-server/src/image/image.module.ts
+++ image-server/src/image/image.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ImageService } from './image.service';
+import { ImageController } from './image.controller';
+import { ProcessorModule } from 'src/processor/processor.module';
+import { MetadataModule } from 'src/metadata/metadata.module';
+import { MinioModule } from 'src/minio/minio.module';
 
-@Module({})
-export class ImageModule {}
+@Module({
+  imports: [ProcessorModule, MetadataModule, MinioModule],
+  providers: [ImageService],
+  controllers: [ImageController],
+})
+export class ImageModule { }
