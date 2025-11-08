a0cdaee32c8a732be006a6e0624268c43f7289d6
YooByWk <tls19190@naver.com>
2025-11-09 00:28:17 +0900

    Feat #2 : 이미지 업로드 및 조회 기능 추가

--- image-server/src/image/image.controller.ts
+++ image-server/src/image/image.controller.ts
@@ -1,4 +1,69 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, NotFoundException, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { ImageService } from './image.service';
+import { MetadataService } from 'src/metadata/metadata.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import type { Response } from 'express';
+import { Readable } from 'stream';
 
 @Controller('image')
-export class ImageController {}
+export class ImageController {
+  constructor(
+    private readonly imageService: ImageService,
+    private readonly metadataService: MetadataService
+  ) { }
+
+  /**
+   * 
+   * @param file 업로드 할 파일
+   * @returns hash - 결과 반환 (이미지 해시)
+   */
+  @Post('upload')
+  @UseInterceptors(FileInterceptor("file"))
+  async uploadImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new NotFoundException("File not provided");
+    }
+    return this.imageService.uploadImage(file);
+  };
+
+  /**
+   * ## `/images/:hash` : 이미지 조회 및 서빙 (정적 호스팅)
+   * ### GET
+   * @param hash 조회할 이미지
+   */
+  @Get(":hash")
+  async getImage(@Param('hash') hash: string, @Res() res: Response) {
+    try {
+      const { stream, mimeType } = await this.imageService.streamImage(hash);
+
+      res.set({
+        'Content-Type': mimeType,
+        'Cache-Control': 'public, max-age=31536000'
+        // todo: 추가 캐싱 검증 로직은 추후
+      });
+
+      (stream as Readable).pipe(res);
+
+    } catch (error) {
+      if (error.message.includes("not found")) {
+        throw new NotFoundException(`Image ${hash} not found`);
+      }
+      throw error;
+    }
+  }
+
+  /**
+   * ## 이미지 삭제
+   * 레퍼런스 카운트를 1 낮춤.
+   * 
+   * *todo*
+   * @todo
+   * @param hash string - 삭제할 이미지의 해시 
+   */
+  @Delete(':hash')
+  async deleteImage(@Param('hash') hash: string) {
+    // await this.metadataService.decrementReferenceCount(hash)
+    // todo
+  }
+
+}
--- image-server/src/image/image.service.ts
+++ image-server/src/image/image.service.ts
@@ -1,4 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, Logger } from '@nestjs/common';
+import { MetadataService } from 'src/metadata/metadata.service';
+import { ImageMetadata } from 'src/metadata/schemas/image-metadata.schema';
+import { MinioService } from 'src/minio/minio.service';
+import { ProcessedImageResult, ProcessorService } from 'src/processor/processor.service';
+import { Readable } from 'stream';
 
 @Injectable()
-export class ImageService {}
+export class ImageService {
+
+  private readonly logger = new Logger(ImageService.name);
+
+  constructor(
+    private readonly metadataService: MetadataService,
+    private readonly minioService: MinioService,
+    private readonly processorService: ProcessorService
+  ) { }
+
+  async uploadImage(file: Express.Multer.File): Promise<{ hash: string, isDuplicate: boolean, objectKey: string; }> {
+    const originalName = file.originalname;
+
+    // 워커 스레드 호출, 이미지 처리
+    const { buffer, hash, mimeType, width, height }: ProcessedImageResult =
+      await this.processorService.processFile(file.buffer);
+
+    // Metadata 확인 -> 중복 체크
+    const existingMeta = await this.metadataService.findByHash(hash);
+    const isDuplicate = !!existingMeta; // boolean 형변환 null, undefined 처리용
+
+    // 중복 케이스
+    if (isDuplicate) {
+      await this.metadataService.incrementReferenceCount(hash);
+      this.logger.log(`Duplicate hash found : ${hash}. Ref count incremented`);
+      return {
+        hash,
+        isDuplicate: true,
+        objectKey: existingMeta.object_key
+      };
+    }
+
+    // 신규 파일 확인
+    const ext = mimeType.split('/')[1];
+    const objectKey = `${hash}.${ext}`;
+    await this.minioService.uploadFile(objectKey, buffer, mimeType);
+    this.logger.log(`New File uploaded object_key : ${objectKey}`);
+
+    // 신규 파일 저장
+    const newMetadata: Partial<ImageMetadata> = await this.metadataService.create({
+      hash_sha256: hash,
+      is_public: true,
+      reference_count: 1,
+      object_key: objectKey,
+      width, height, mime_type: mimeType,
+    });
+
+    return { hash, isDuplicate: false, objectKey: newMetadata.object_key! };
+  }
+
+
+  /**
+   * ## 정적 호스팅을 통한 이미지 조회
+   * @param {string} hash string : 조회할 이미지의 해시
+   * @returns 
+   */
+  async streamImage(hash: string): Promise<{ stream: Readable, mimeType: string; }> {
+    const meta = await this.metadataService.findByHash(hash);
+
+    if (!meta) {
+      throw new ConflictException(`Image ${hash} not found`);
+    }
+    const stream = await this.minioService.getFileStream(meta.object_key);
+
+    return { stream, mimeType: meta.mime_type };
+  }
+
+  // todo: delete/decrementReferenceCount
+} // ImageService Class ends 
